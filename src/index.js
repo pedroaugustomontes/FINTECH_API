e51647bc23454c4cb59a4c9d78098156d7c25b1e
Pedro Augusto Montes <nerdindiano@gmail.com>
2022-07-19 19:25:45 -0300

    criado POST que realiza deposito em uma conta existente

--- src/index.js
+++ src/index.js
@@ -7,19 +7,19 @@ app.use(express.json());
 // Array customers simulando banco de dados em memória
 const customers = [];
 
-// Middleware
+// Middleware para verificar se existe conta
 function verifyIfExistsAccountCPF(request, response, next) {
     const { cpf } = request.headers;
     const customer = customers.find(customer => customer.cpf === cpf);
 
-    //codigo que não deixa ser possivel buscar extrato em uma conta não existente
+    //codigo que não deixa ser possivel prosseguir se o cpf não for encontrado no db
     if (!customer) {
         return response.status(400).json(
             {erro:'Cliente não encontrado!'}
         )
     };
 
-    // onde o middleware foi chamado irá ter acesso ao customer(CPF)
+    // onde o middleware for chamado irá ter acesso ao customer(CPF)
     request.customer = customer;
 
     return next();
@@ -51,6 +51,24 @@ app.post("/account", (request, response) => {
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
     const { customer } = request;
     return response.json(customer.statement);
+});
+
+// REQUISITO 3, DEVE SER POSSIVEL REALIZAR UM DEPOSITO
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+    // codigo abaixo que cria um objeto com as infos do deposito e o insere no cliente desestruturado pelo request do middleware
+    const statementOperation = {
+        description,
+        amount,
+        date: new Date(),
+        type: "credit"
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
 });
 
 app.listen(7474)
